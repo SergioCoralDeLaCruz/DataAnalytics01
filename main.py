951a8109d35419882b0211d4bcd6ab13068221f7
Carlos Ramos <carlos_ramosp@unicomer.com>
2024-07-19 22:09:39 -0500

    se cambio main por nombre de funcion correcto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from exploracionDx import tratamientoDx
+from tratamientoDx import drop_nulls
 
 def main():
     data = {'A': [1, 2, None, 4], 'B': [5, None, -7, 8], 'C': [9, 10, 11, 12]}
@@ -7,7 +7,7 @@ def main():
     
     print(df)
     
-    df_clean = tratamientoDx(df)
+    df_clean = drop_nulls(df)
     
     print("\nDataFrame limpio:")
     print(df_clean)
