25bc62ddc3c366f554bff78d87254b5506583be3
Sergio Eduardo Coral De La Cruz <sergio.coral@pucp.pe>
2024-07-19 22:12:52 -0500

    Correlación entre pares de variables

---
+++ exploracionDx.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_correlacion(df):
+    # Obtener las columnas numéricas
+    columnas_numericas = df.select_dtypes(include='number').columns
+
+    # Calcular la matriz de correlación de Pearson
+    matriz_correlacion = df[columnas_numericas].corr(method='pearson')
+
+    # Graficar la matriz de correlación
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(matriz_correlacion, annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
+    plt.title('Matriz de Correlación de Pearson')
+    plt.show()
